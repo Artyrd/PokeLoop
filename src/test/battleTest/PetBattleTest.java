c337b746900f59d60e80c7fa1d5aac4f41a05a73
Daniel Dang <danielngocdang-99@gmail.com>
2021-08-05 16:15:32 +1000

    init

---
+++ src/test/battleTest/PetBattleTest.java
@@ -0,0 +1,23 @@
+package test.battleTest;
+
+import org.junit.Test;
+
+public class PetBattleTest {
+    
+    @Test
+    public void testAttack(){
+        
+    }
+
+    @Test
+    public void testCritAttackStun(){
+
+    }
+
+    @Test
+    public void testCritAttackBurned(){
+
+    }
+
+
+}
